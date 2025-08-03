4fb109a6be7b8cf5ab1fb68c4173ea2ab1ca7d6c
jime2803 <mije82@gmail.com>
2025-08-03 01:08:20 -0300

    add: layout terminado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer style={{ background: "lightcoral" }}>
+      <p>Sitio desarrollado por
+        <a href="https://www.linkedin.com/in/jimena-nora-sastre-b16a0977/" target="blank">Linkedin de Jimena Sastre</a>
+      </p>
+    </footer>
+  )
+}
+
+export { Footer }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header style={{ background: "lighblue" }}>
+      <img src="http://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="Imagen de Logo" />
+      <nav>
+        <ul>
+          {/* Cambiar elementos a por componentes Link to de react-router-dom. De esta manera el navegador no se refresca y solo cambia de un componente a otro  */}
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/registrate">Registrate</a></li>
+          <button>Cerrar sesión</button>
+        </ul>
+      </nav>
+    </header >
+  )
+}
+
+export { Header }
