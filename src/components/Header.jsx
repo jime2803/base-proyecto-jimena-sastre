c27dd5f8b7a3aa8c889f37dbaaeca2d7e70f0f2c
jime2803 <mije82@gmail.com>
2025-08-03 02:44:25 -0300

    Add: Header and Nav styles update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 const Header = () => {
   return (
     <header className="header">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6r66wSgNLORR5YsgEqjdHTxpbRq7afXJPow&s" alt="Logo" />
-      <nav className="nav">
-        <ul className="nav-list">
-          {/* Cambiar elementos a por componentes Link to de react-router-dom. De esta manera el navegador no se refresca y solo cambia de un componente a otro  */}
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/login">Login</a></li>
-          <li><a href="/registrate">Registrate</a></li>
-          <button>Cerrar sesión</button>
-        </ul>
+      <nav className="nav-bar">
+        <div className="nav-content">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlMB8QIUMDZGt49epmEkVmaXzfLiraPRfMJA&s" alt="Logo" className="logo" />
+          <ul className="nav-list">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/registrate">Registrate</a></li>
+            <li>
+              <button className="logout-button">Cerrar sesión</button>
+            </li>
+          </ul>
+        </div>
       </nav>
-    </header >
+    </header>
   )
 }
 
