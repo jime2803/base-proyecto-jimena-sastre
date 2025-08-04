c54aa01b28564cf40cc9fd628287221feada0e98
jime2803 <mije82@gmail.com>
2025-08-04 00:38:20 -0300

    Add: fetching async funtion and useEffect, partially

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,30 @@
 import { Layout } from "../components/Layout"
+import "../styles/pages/Home.css"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+
+  const [products, setProducts] = useState([])
+
+  const fetchingProducts = async () => {
+    const response = await fetch("http://fakestoreapi.com/Products", { method: "GET" })
+    const data = await response.json()
+
+    /*esta es una funcion asincrónica (demora porque nosotros no tenemos el control del tiempo), para esperar que el fetch me traiga (GET) la respuesta. si en lugar de usar el metodo GET usara el metodo POST, entenderia que estoy intentando "enviar" un producto. hay otros metodos: PUSH, PUT, DELETE.. */
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchingProducts()
+  }, [])
+  /* useEffect es una funcion y espera dos parametros: un callback-una funcion y un array-Lista de dependencias, separados por una coma*/
+  /*el Array vacio (dependencias) espera a que ejecute el return del jsx. si tiene algo, useEffect se va a ejecutar cada vez que se modifique lo que este adentro de la dependencia.*/
+
+
+
+  /*Voy a pedir al react que primero muestre en el navegador la visual de lo que hay en return mientras espera que el fecth me traiga la data (primero muestra el return y luego ejecuta la funcion) - se aplica un efecto secundario --> un hook (use) */
+
   return (
     <Layout>
       <section>
@@ -28,11 +52,14 @@ const Home = () => {
         <h2>Nuestros productos</h2>
         <p>Elegí nuestras categorías más populares.</p>
 
-        {/* {Seccion reservada para carga diferida de productos} */}
+        {
+          products.map((product) => <h2></h2>)
+        }
 
-        <div className="product-loading">
+        {/* map es como el forEach que itera cada elemento  */}
+        {/* <div className="product-loading">
           <p>Cargando productos...</p>
-        </div>
+        </div> */}
       </section>
     </Layout>
   )
